d452f4da2405f2bbf467e28bec05196087691a58
AlexBotaefja <112579750+AlexBotaefja@users.noreply.github.com>
2022-09-09 00:21:28 -0600

    Update close.js

--- execute/close.js
+++ execute/close.js
@@ -10,7 +10,7 @@ module.exports = async (user, channel) => {
   let embed = new discord.MessageEmbed()
   .setTitle("Ticket Cerrado <a:_:1016358279938588672>")
   .setColor("RANDOM")
-  .setDescription("Su boleto ha sido cerrado ┊ \ Responder ahora creará un nuevo ticket.<a:_:1013316019936296980> \n\nNo dude en ponerse en contacto con nosotros de nuevo si necesita cualquier tipo de ayuda.Gracias por utilizar nuestro servicio ModMail.")
+  .setDescription("Su boleto ha sido cerrado ┊ \ Responder ahora creará un nuevo ticket.<a:_:1014513043675291739> \n\nNo dude en ponerse en contacto con nosotros de nuevo si necesita cualquier tipo de ayuda.Gracias por utilizar nuestro servicio ModMail.")
   .setTimestamp();
 
   if (userdata !== null) {
