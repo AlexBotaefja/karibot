585fe46b6d7b9d5e21a2f31b1c15e7e7f04c6a79
AlexBotaefja <112579750+AlexBotaefja@users.noreply.github.com>
2022-09-09 09:58:16 -0600

    Update senddm.js

--- execute/senddm.js
+++ execute/senddm.js
@@ -5,7 +5,7 @@ module.exports = async (message, args, user, channel, anonymous, client) => {
   let embed = new discord.MessageEmbed()
   .setDescription(args.join(" "))
   .setColor("#96f7b0")
-  .setFooter("El mal uso de este medio conllevará una sanción ┊ El staff nunca te preguntará por tu correo, contraseña, números telefónicos y/o tokens '+message.author.avatarURL()'")
+  .setFooter(message.author.avatarURL() "El mal uso de este medio conllevará una sanción ┊ El staff nunca te preguntará por tu correo, contraseña, números telefónicos y/o tokens")
   
   if (message.attachments.array().length > 0) {
      embed.setImage(message.attachments.array()[0].url)
